46419e76ee59eb50b31381c354a9c1feab249286
Haoyu Miao <47076619+zjumhy97@users.noreply.github.com>
2022-04-20 22:16:54 +0800

    complete EVSPGraphVehicleNode.py

--- evsp_virtual_vehicle_nodes/test.py
+++ evsp_virtual_vehicle_nodes/test.py
@@ -1,6 +1,7 @@
 import network_haoyu.EVSPGraphVehicleNode as EGraph
 
 import time
+import json
 
 soc_ev = {'粤B12345':100, '粤B23456':90, '粤B34567':80, '粤B45678':70}
 start_time_str = '2022-03-24 06:50:00'
@@ -8,7 +9,17 @@ start_time = int(time.mktime(time.strptime(start_time_str, "%Y-%m-%d %H:%M:%S"))
 end_time_str = '2022-03-25 06:30:00'
 end_time = int(time.mktime(time.strptime(end_time_str, "%Y-%m-%d %H:%M:%S")))
 
-graph = EGraph.EVSPGraph(time_granularity=5, soc_ev=soc_ev)
+trip_number = 6
+gap = 15
+duration = 45
+
+file = open('./trip_data/trip_' + str(trip_number) + '_gap_'+ str(gap) +'_dura_'
+                + str(duration)+'.json', 'r')
+trip_data = json.load(file)
+
+
+
+graph = EGraph.EVSPGraph(time_granularity=5, soc_ev=soc_ev, trip_info=trip_data)
 
 graph.start_time = start_time
 graph.start_time_str = start_time_str
@@ -17,7 +28,7 @@ graph.end_time_str = end_time_str
 
 # graph.addDepotVertex(attribute='origin')
 # graph.addVirtualVehicleVertex(soc_ev=soc_ev)
-graph.constructGraph(vertex_info=None)
+graph.construct_graph(vertex_info=None)
 
 
 print(1)
--- network_haoyu/EVSPGraphVehicleNode.py
+++ network_haoyu/EVSPGraphVehicleNode.py
@@ -4,7 +4,7 @@ import network_haoyu.Graph as Graph
 # 3 class vertex: trip vertex, origin/destination vertex, virtual vehicle vertex
 
 class EVSPGraph(Graph.Graph):
-    def __init__(self, time_granularity, soc_ev):
+    def __init__(self, time_granularity, soc_ev, trip_info):
         super(EVSPGraph, self).__init__()
         # 时间粒度这里可能有问题，具体用到这个变量的时候再 check 一下
         # time_granularity > 1
@@ -16,39 +16,43 @@ class EVSPGraph(Graph.Graph):
         self.end_time = None
         self.end_time_str = None
         self.pull_out_edge_list = []
+        self.trip_info = trip_info
 
-    def addVertex(self, vertex_list):
+    def add_vertex(self, id, attribute, start_time_str, duration):
         # 向 vertex_set 中添加 vertex
-        vertex = EVSPVertex(vertex_id=vertex_list[0], vertex_attribute=vertex_list[1],
-                           start_time_str=vertex_list[2], duration=vertex_list[3])
+        vertex = EVSPVertex(vertex_id=id, vertex_attribute=attribute, start_time_str=start_time_str, duration=duration)
         self.vertex_set.append(vertex)
+        self.update()
         return vertex
 
-    def addTripVertex(self):
-        # 还没完成
-        vertex_list = []
-        vertex = self.addVertex(vertex_list=vertex_list)
-        self.addEdge(vertex=vertex, attribute='trip')
-        self.update()
+    def add_trip_vertex(self):
+        # [3, 'task', '2022-03-24 07:30:00', 45]
+        trip_number = len(self.trip_info)
+        for i in range(1, trip_number+1):
+            trip = self.trip_info['trip' + str(i)]
+            vertex = self.add_vertex(id=trip['trip_id'], attribute=trip['attribute'],
+                                    start_time_str=trip['start_time_str'], duration=trip['duration'])
+            self.add_edge(vertex=vertex, attribute='normal')
         return 0
 
-    def addDepotVertex(self, attribute):
+    def add_depot_vertex(self, attribute):
         # depot vertex duration > 0, here we set the duration to 1 min
         if attribute == 'origin':
             # [3, 'task', '2022-03-24 07:30:00', 45]
-            vertex_list = [0, 'origin', self.start_time_str, 1]
-            vertex_origin = self.addVertex(vertex_list=vertex_list)
+            vertex_origin = self.add_vertex(id=0, attribute='origin',
+                                           start_time_str=self.start_time_str, duration=1)
         elif attribute == 'destination':
-            vertex_list = [900000, 'destination', self.end_time_str, 1]
-            vertex_destination = self.addVertex(vertex_list=vertex_list)
+            # vertex_list = [900000, 'destination', self.end_time_str, 1]
+            vertex_destination = self.add_vertex(id=900000, attribute='destination',
+                                                start_time_str=self.end_time_str, duration=1)
             # add the edge compatible with vertex_destination
-            self.addEdge(vertex=vertex_destination, attribute='pull in')
+            self.add_edge(vertex=vertex_destination, attribute='pull in')
         else:
             print('EVSPGraphVehicleNode.py -- addDepotVertex attribute input must be ‘origin’ or ‘destination’!')
         self.update()
         return 0
 
-    def addVirtualVehicleVertex(self, soc_ev):
+    def add_virtual_vehicle_vertex(self, soc_ev):
         # 除了添加 vehicle vertex， 也添加了 pull out edge
         # soc_ev = {'粤B12345':100, '粤B23456':100, '粤B34567':100, '粤B45678':100}
         # 添加 virtual vehicle node 需要添加 ev 的 soc
@@ -63,11 +67,11 @@ class EVSPGraph(Graph.Graph):
                 # [3, 'task', '2022-03-24 07:30:00', 45]
                 start_time = self.start_time + vertex_origin.duration
                 start_time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))
-                vertex_list = [i+100001, vehicle, start_time_str, 1]
-                vertex_vehicle = self.addVertex(vertex_list=vertex_list)
+                # vertex_list = [i+100001, vehicle, start_time_str, 1]
+                vertex_vehicle = self.add_vertex(id=i+100001, attribute=vehicle, start_time_str=start_time_str, duration=1)
                 vertex_vehicle.soc = soc
                 i += 1
-                self.addEdge(vertex=vertex_vehicle, attribute='pull out')
+                self.add_edge(vertex=vertex_vehicle, attribute='pull out')
             # update the pull_out_edge_list of this graph
             edge_id = len(self.edge_set)
             self.pull_out_edge_list.extend([i for i in range(edge_id)])
@@ -76,7 +80,7 @@ class EVSPGraph(Graph.Graph):
         self.update()
         return 0
 
-    def addEdge(self, vertex, attribute):
+    def add_edge(self, vertex, attribute):
         # 根据时间判断，能否与已有节点创建有向边，add time compatible edge
         vertex_set = self.vertex_set
         edge_id = len(self.edge_set)
@@ -96,18 +100,17 @@ class EVSPGraph(Graph.Graph):
         self.update()
         return 0
 
-    def constructGraph(self, vertex_info):
+    def construct_graph(self, vertex_info):
         # construct graph process:
         # 1. add origin vertex
-        self.addDepotVertex(attribute='origin')
+        self.add_depot_vertex(attribute='origin')
         # 2. add virtual vehicle vertex
-        self.addVirtualVehicleVertex(soc_ev=self.soc_ev)
+        self.add_virtual_vehicle_vertex(soc_ev=self.soc_ev)
         # 3. add trip vertex
-        self.addTripVertex()
+        self.add_trip_vertex()
         # 4. add destination vertex
-        self.addDepotVertex(attribute='destination')
-        pass
-
+        self.add_depot_vertex(attribute='destination')
+        return 0
 
 
 class EVSPVertex(Graph.Vertex):
@@ -125,4 +128,4 @@ class EVSPEdge(Graph.Edge):
     def __init__(self, edge_id, attribute, end1, end2, directed=True, weight=0):
         super(EVSPEdge, self).__init__(vertex_end1=end1, vertex_end2=end2, directed=directed, weight=weight)
         self.edge_id = edge_id
-        self.attribute = attribute # attribute: pull out, trip, pull in
+        self.attribute = attribute # attribute: pull out, normal, pull in
