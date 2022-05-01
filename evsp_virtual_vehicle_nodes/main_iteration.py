730a3e5ea8743389ae46405a1012c9465015619c
Haoyu Miao <47076619+zjumhy97@users.noreply.github.com>
2022-05-01 22:20:43 +0800

    construct the iteration while loop

--- evsp_virtual_vehicle_nodes/main_iteration.py
+++ evsp_virtual_vehicle_nodes/main_iteration.py
@@ -7,9 +7,7 @@ import copy
 import sys
 import time
 
-
-
-trip_num = 193
+trip_num = 50 # 193 50
 soc_ev = {'粤B12345': 100,
           '粤B13456': 90,
           '粤B14567': 80,
@@ -20,60 +18,77 @@ soc_ev = {'粤B12345': 100,
           '粤B19012': 80,
           '粤B10123': 70,
           '粤B11234': 70,
-          '粤B22345': 100,
-          '粤B23456': 90,
-          '粤B24567': 80,
-          '粤B25678': 70,
-          '粤B26789': 80,
-          '粤B27890': 70,
-          '粤B28901': 70,
-          '粤B29012': 80,
-          '粤B20123': 70,
-          '粤B21234': 70,
-          '粤B32345': 100,
-          '粤B33456': 90,
-          '粤B34567': 80,
-          '粤B35678': 70,
-          '粤B36789': 80,
-          '粤B37890': 70,
-          '粤B38901': 70,
-          '粤B39012': 80,
-          '粤B30123': 70,
-          '粤B31234': 70,
-          '粤B42345': 100,
-          '粤B43456': 90,
-          '粤B44567': 80,
-          '粤B45678': 70,
-          '粤B46789': 80,
-          '粤B47890': 70,
-          '粤B48901': 70,
-          '粤B49012': 80,
-          '粤B40123': 70,
-          '粤B51234': 70,
+          # '粤B22345': 100,
+          # '粤B23456': 90,
+          # '粤B24567': 80,
+          # '粤B25678': 70,
+          # '粤B26789': 80,
+          # '粤B27890': 70,
+          # '粤B28901': 70,
+          # '粤B29012': 80,
+          # '粤B20123': 70,
+          # '粤B21234': 70,
+          # '粤B32345': 100,
+          # '粤B33456': 90,
+          # '粤B34567': 80,
+          # '粤B35678': 70,
+          # '粤B36789': 80,
+          # '粤B37890': 70,
+          # '粤B38901': 70,
+          # '粤B39012': 80,
+          # '粤B30123': 70,
+          # '粤B31234': 70,
+          # '粤B42345': 100,
+          # '粤B43456': 90,
+          # '粤B44567': 80,
+          # '粤B45678': 70,
+          # '粤B46789': 80,
+          # '粤B47890': 70,
+          # '粤B48901': 70,
+          # '粤B49012': 80,
+          # '粤B40123': 70,
+          # '粤B51234': 70,
           }
 vehicle_num = len(soc_ev)
 graph_file_path = './graph_generated/'
+pickle_file_name = 'vehicle_10_trip_50.pickle'
+# pickle_file_name = 'vehicle_40_trip_193.pickle'
+# pickle_file_name = 'vehicle_4_trip_6.pickle'
 
-# if os.path.exists(graph_file_path + 'vehicle_4_trip_6.pickle'):
-#     with open(graph_file_path + 'vehicle_4_trip_6.pickle', 'rb') as f:
-#         graph = pickle.load(f)
-#         graph = graph[0]
+def load_graph(graph_file_path:str, pickle_file_name:str):
+    """
+    Load the EVSP graph from .pickle file located in the file path.
 
-if os.path.exists(graph_file_path + 'vehicle_40_trip_193.pickle'):
-    with open(graph_file_path + 'vehicle_40_trip_193.pickle', 'rb') as f:
-        graph = pickle.load(f)
-        graph = graph[0]
+    :param graph_file_path: the file path of the .pickle file
+    :param pickle_file_name: the name of the .pickle file
+    :return: the EVSP graph
+    """
+    if os.path.exists(graph_file_path + pickle_file_name):
+        with open(graph_file_path + pickle_file_name, 'rb') as f:
+            graph = pickle.load(f)
+            graph = graph[0]
+    return graph
 
-vertex_size, edge_size = graph.graphSize()
-time_slot_num = int(24 * 60 / graph.time_granularity)
-s_upper = 100
-s_lower = 10
-M = 1000000
-q_upper = 5
-q_total_upper = 5 * 20
+def path_variable_binary_check():
+    """
+    Check whether the path variable value solved from master problem (linear relaxation) is binary.
 
-# 打印 path
+    :return: The truth value
+    """
+    result_not_int = False
+    for i in range(edge_size):
+        if x[i].x != 1 and x[i].x != 0:
+            result_not_int = True
+    if result_not_int:
+        print('*******************  Result not integer!  *******************')
+    return result_not_int
+
 def print_eb_path():
+    """
+    Print the path for all of the EBs after solving the master problem.
+
+    :return: 0
+    """
     for k in range(vehicle_num):
         print('EB_' + str(k) + ' ' + graph.vertex_set[k+1].vertex_attribute + ' soc =' + str(graph.vertex_set[k+1].soc) )
         path_k = []
@@ -88,17 +103,14 @@ def print_eb_path():
                         next_vertex_id = graph.edge_set[i].end2.vertex_id
                         break
         print("(", len(path_k) - 3, ")", path_k)
-
-def path_variable_binary_check():
-    result_not_int = False
-    for i in range(edge_size):
-        if x[i].x != 1 and x[i].x != 0:
-            result_not_int = True
-
-    if result_not_int:
-        print('*******************  Result not integer!  *******************')
+    return 0
 
 def print_eb_path_with_soc_charging():
+    """
+    Print the path of EBs with the soc variation after solving the subproblem.
+
+    :return: 0
+    """
     for k in range(vehicle_num):
         print('EB_' + str(k) + ' ' + graph.vertex_set[k+1].vertex_attribute + ' soc =' + str(graph.vertex_set[k+1].soc) )
         path_k = []
@@ -113,137 +125,194 @@ def print_eb_path_with_soc_charging():
                         # 想要的 [45, '74', 15] -> [15+25, '83', 10]
                         path_k.append([s[graph.edge_set[i].end1.vertex_id].x + c[i].x,
                                        str(graph.edge_set[i].end2.vertex_id),
-                                       s[graph.edge_set[i].end2.vertex_id].x])
+                                       s[graph.edge_set[i].end2.vertex_id].x,
+                                       c[i].x])
                         next_vertex_id = graph.edge_set[i].end2.vertex_id
                         break
         print("(", len(path_k) - 3, ")", path_k)
+    return 0
 
-# Master Problem
-MP = gp.Model()
-x = MP.addVars(edge_size, vtype=GRB.CONTINUOUS, name="x")
+graph = load_graph(graph_file_path=graph_file_path, pickle_file_name=pickle_file_name)
 
-b = np.array([vehicle_num])
-b = np.append(b, np.zeros(vertex_size-2))
-b = np.append(b, [-vehicle_num])
-# b = np.array([1] + [0 for i in range(len(vertex_info)-1)] + [-1])
+vertex_size, edge_size = graph.graphSize()
+time_slot_num = int(24 * 60 / graph.time_granularity)
+s_upper = 100
+s_lower = 10
+M = 1000000
+q_upper = 5
+q_total_upper = 5 * 10
+SolvedFlag = False
 
-# flow constraints
-for j in range(vertex_size):
-    MP.addConstr(gp.quicksum(x[i] for i in graph.vertex_set[j].edge_id_out) -
-                    gp.quicksum(x[i] for i in graph.vertex_set[j].edge_id_in) == b[j])
+iteration = 0
+while SolvedFlag is False:
+    # Master Problem
+    MP = gp.Model()
+    x = MP.addVars(edge_size, vtype=GRB.CONTINUOUS, name="x")
 
-# exactly once constraints
-for j in range(1, vertex_size-1):
-    MP.addConstr(gp.quicksum(x[i] for i in graph.vertex_set[j].edge_id_out) == 1)
+    b = np.array([vehicle_num])
+    b = np.append(b, np.zeros(vertex_size-2))
+    b = np.append(b, [-vehicle_num])
+    # b = np.array([1] + [0 for i in range(len(vertex_info)-1)] + [-1])
 
-MP.setObjective(gp.quicksum(x[i] for i in range(graph.edge_size)), GRB.MAXIMIZE)
-MP.optimize()
-path_variable_binary_check()
-print_eb_path()
+    # flow constraints
+    for j in range(vertex_size):
+        MP.addConstr(gp.quicksum(x[i] for i in graph.vertex_set[j].edge_id_out) -
+                        gp.quicksum(x[i] for i in graph.vertex_set[j].edge_id_in) == b[j])
 
-# Subproblem
-SP = gp.Model()
+    # exactly once constraints
+    for j in range(1, vertex_size-1):
+        MP.addConstr(gp.quicksum(x[i] for i in graph.vertex_set[j].edge_id_out) == 1)
 
-s = SP.addVars(vertex_size, vtype=GRB.CONTINUOUS, name="s")
-w = SP.addVars(edge_size, lb=0, vtype=GRB.CONTINUOUS, name="w")
-m = SP.addVars(edge_size, lb=0, vtype=GRB.CONTINUOUS, name="m")
-c = SP.addVars(edge_size, lb=0, vtype=GRB.CONTINUOUS, name="c")
-q = SP.addVars(edge_size, time_slot_num, lb=0, ub=q_upper, vtype=GRB.CONTINUOUS, name='q')
+    MP.setObjective(gp.quicksum(x[i] for i in range(graph.edge_size)), GRB.MAXIMIZE)
+    MP.optimize()
+    path_variable_binary_check()
+    print_eb_path()
 
-# soc constraints
-SP.addConstr(s[0] == s_upper)
-for k in range(vehicle_num):
-    SP.addConstr(s[k+1] == graph.vertex_set[k+1].soc)
-for j_index in range(trip_num):
-    j = vehicle_num + 1 + j_index
-    SP.addConstr(s[j] >= s_lower)
-    SP.addConstr(s[j] <= s_upper - graph.vertex_set[j].energy_consumption)
-SP.addConstr(s[vertex_size - 1] == s_upper * vehicle_num)
+    # Subproblem
+    SP = gp.Model()
 
-# soc evolution
-for j in range(vehicle_num + 1, vertex_size):
-    e_j = graph.vertex_set[j].energy_consumption
-    SP.addConstr(s[j] == gp.quicksum(w[i] + m[i] - x[i].x * e_j for i in graph.vertex_set[j].edge_id_in))
+    s = SP.addVars(vertex_size, vtype=GRB.CONTINUOUS, name="s")
+    w = SP.addVars(edge_size, lb=0, vtype=GRB.CONTINUOUS, name="w")
+    m = SP.addVars(edge_size, lb=0, vtype=GRB.CONTINUOUS, name="m")
+    c = SP.addVars(edge_size, lb=0, vtype=GRB.CONTINUOUS, name="c")
+    q = SP.addVars(edge_size, time_slot_num, lb=0, ub=q_upper, vtype=GRB.CONTINUOUS, name='q')
 
-# bilinear constraints
-for i in range(edge_size):
-    SP.addConstr(w[i] <= M * x[i].x)
-    SP.addConstr(w[i] >= s[graph.edge_set[i].end1.vertex_id] - M * (1 - x[i].x))
-    SP.addConstr(w[i] <= s[graph.edge_set[i].end1.vertex_id] + M * (1 - x[i].x))
+    # soc constraints
+    SP.addConstr(s[0] == s_upper)
+    for k in range(vehicle_num):
+        SP.addConstr(s[k+1] == graph.vertex_set[k+1].soc)
+    for j_index in range(trip_num):
+        j = vehicle_num + 1 + j_index
+        SP.addConstr(s[j] >= s_lower)
+        SP.addConstr(s[j] <= s_upper - graph.vertex_set[j].energy_consumption)
+    SP.addConstr(s[vertex_size - 1] <= s_upper * vehicle_num)
+    # SP.addConstr(s[vertex_size - 1] == s_upper * vehicle_num)
 
-    SP.addConstr(m[i] <= M * x[i].x)
-    SP.addConstr(m[i] >= c[i] - M * (1 - x[i].x))
-    SP.addConstr(m[i] <= c[i] + M * (1 - x[i].x))
+    # soc evolution
+    for j in range(vehicle_num + 1, vertex_size):
+        e_j = graph.vertex_set[j].energy_consumption
+        SP.addConstr(s[j] == gp.quicksum(w[i] + m[i] - x[i].x * e_j for i in graph.vertex_set[j].edge_id_in))
 
-    SP.addConstr(w[i] + m[i] <= s_upper)
+    # bilinear constraints
+    for i in range(edge_size):
+        SP.addConstr(w[i] <= M * x[i].x)
+        SP.addConstr(w[i] >= s[graph.edge_set[i].end1.vertex_id] - M * (1 - x[i].x))
+        SP.addConstr(w[i] <= s[graph.edge_set[i].end1.vertex_id] + M * (1 - x[i].x))
+
+        SP.addConstr(m[i] <= M * x[i].x)
+        SP.addConstr(m[i] >= c[i] - M * (1 - x[i].x))
+        SP.addConstr(m[i] <= c[i] + M * (1 - x[i].x))
+
+        SP.addConstr(w[i] + m[i] <= s_upper)
+
+    # charging constraints
+    for i in range(edge_size):
+        initial_time = graph.start_time
+        toi = graph.edge_set[i].end1.start_time
+        doi = graph.edge_set[i].end1.duration
+        tdi = graph.edge_set[i].end2.start_time
+        start_index = int((toi + doi - initial_time)/graph.time_granularity / 60)
+        end_index = int((tdi - initial_time)/graph.time_granularity / 60)
+        # constraint (1h)
+        # key constraint, whether the charging amount could be achieved physically
+        SP.addConstr(c[i] == gp.quicksum(q[i, t] for t in range(start_index, end_index)))
+
+    # constraint (1j)
+    for t in range(time_slot_num):
+        SP.addConstr(gp.quicksum(q[i, t] for i in range(edge_size)) <= q_total_upper)
+
+    # electricity_price
+    p = []
+    for t in range(time_slot_num):
+        time_temp = graph.start_time + t * graph.time_granularity * 60
+        time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time_temp))
+        p.append(graph.electricity_price(time_str, mode='TOU'))
+
+    # 假设 5% 对应的是 5度电
+    objective = gp.quicksum(p[t] * gp.quicksum(q[i, t] for i in range(edge_size)) for t in range(time_slot_num))
+    # objective = gp.quicksum(p[t] * gp.quicksum(q[i, t] for i in range(edge_size)) for t in range(time_slot_num)) \
+    #             - s[vertex_size - 1]
+
+    SP.setObjective(objective, GRB.MINIMIZE)
+
+    # timer_sp_optimize_start = time.time()
+    SP.optimize()
+    # timer_sp_optimize_end = time.time()
+    # print('time (subproblem optimization) = ', timer_sp_optimize_end - timer_sp_optimize_start)
+    if SP.Status == 2:
+        # optimal
+        print_eb_path_with_soc_charging()
+        SolvedFlag = True
+    elif SP.Status == 3:
+        # infeasible
+        print('>> Subproblem is infeasible.')
+        # solve Problem 4a, subproblem_relaxed -> SPR
+        # Subproblem Relaxation
+        SPR = gp.Model()
+
+        s = SPR.addVars(vertex_size, vtype=GRB.CONTINUOUS, name="s")
+        w = SPR.addVars(edge_size, lb=0, vtype=GRB.CONTINUOUS, name="w")
+        m = SPR.addVars(edge_size, lb=0, vtype=GRB.CONTINUOUS, name="m")
+        c = SPR.addVars(edge_size, lb=0, vtype=GRB.CONTINUOUS, name="c")
+        q = SPR.addVars(edge_size, time_slot_num, lb=0, ub=q_upper, vtype=GRB.CONTINUOUS, name='q')
+
+        # soc constraints
+        SPR.addConstr(s[0] == s_upper)
+        for k in range(vehicle_num):
+            SPR.addConstr(s[k + 1] == graph.vertex_set[k + 1].soc)
+        for j_index in range(trip_num):
+            j = vehicle_num + 1 + j_index
+            SPR.addConstr(s[j] >= s_lower)
+            SPR.addConstr(s[j] <= s_upper - graph.vertex_set[j].energy_consumption)
+        SPR.addConstr(s[vertex_size - 1] <= s_upper * vehicle_num)
+        # SPR.addConstr(s[vertex_size - 1] == s_upper * vehicle_num)
+
+        # soc evolution
+        for j in range(vehicle_num + 1, vertex_size):
+            e_j = graph.vertex_set[j].energy_consumption
+            SPR.addConstr(s[j] == gp.quicksum(w[i] + m[i] - x[i].x * e_j for i in graph.vertex_set[j].edge_id_in))
+
+        # bilinear constraints
+        for i in range(edge_size):
+            SPR.addConstr(w[i] <= M * x[i].x)
+            SPR.addConstr(w[i] >= s[graph.edge_set[i].end1.vertex_id] - M * (1 - x[i].x))
+            SPR.addConstr(w[i] <= s[graph.edge_set[i].end1.vertex_id] + M * (1 - x[i].x))
+
+            SPR.addConstr(m[i] <= M * x[i].x)
+            SPR.addConstr(m[i] >= c[i] - M * (1 - x[i].x))
+            SPR.addConstr(m[i] <= c[i] + M * (1 - x[i].x))
+
+            SPR.addConstr(w[i] + m[i] <= s_upper)
+
+        # electricity_price
+        p = []
+        for t in range(time_slot_num):
+            time_temp = graph.start_time + t * graph.time_granularity * 60
+            time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time_temp))
+            p.append(graph.electricity_price(time_str, mode='TOU'))
+
+        # 假设 5% 对应的是 5度电
+        objective = gp.quicksum(p[t] * gp.quicksum(q[i, t] for i in range(edge_size)) for t in range(time_slot_num))
+        # objective = gp.quicksum(p[t] * gp.quicksum(q[i, t] for i in range(edge_size)) for t in range(time_slot_num)) \
+        #             - s[vertex_size - 1]
+
+        SPR.setObjective(objective, GRB.MINIMIZE)
+
+        # timer_sp_optimize_start = time.time()
+        SPR.optimize()
+        # timer_sp_optimize_end = time.time()
+        # print('time (subproblem optimization) = ', timer_sp_optimize_end - timer_sp_optimize_start)
+        print_eb_path_with_soc_charging()
+
+        # violation check
 
 
-# charging constraints
-for i in range(edge_size):
-    initial_time_str = graph.edge_set[i].end1.start_time_str[0:11] + '00:00:00'
-    initial_time = int(time.mktime(time.strptime(initial_time_str, "%Y-%m-%d %H:%M:%S")))
-    toi = graph.edge_set[i].end1.start_time
-    doi = graph.edge_set[i].end1.duration
-    index = int((toi + doi - initial_time)/graph.time_granularity / 60)
-    SP.addConstr(c[i] == gp.quicksum(q[i, t] for t in range(index, time_slot_num)))
+        # trip duration modification
 
+        # graph update
 
-for t in range(time_slot_num):
-    SP.addConstr(gp.quicksum(q[i, t] for i in range(edge_size)) <= q_total_upper)
-
-# electricity_price
-p = []
-for t in range(time_slot_num):
-    time_temp = graph.start_time + t * graph.time_granularity * 60
-    time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time_temp))
-    p.append(graph.electricity_price(time_str, mode='TOU'))
-
-# 假设 5% 对应的是 5度电
-objective = gp.quicksum(p[t] * gp.quicksum(q[i, t] for i in range(edge_size)) for t in range(time_slot_num))
-SP.setObjective(objective, GRB.MINIMIZE)
-# SP.setObjective(gp.quicksum(x[i].x * graph.edge_set[i].weight for i in range(edge_size)), GRB.MINIMIZE)
-SP.optimize()
-# timer_optimize_start = time.time()
-# model.optimize()
-# timer_optimize_end = time.time()
-
-# model.write(".sol")
-
-print_eb_path_with_soc_charging()
-
-
-# print(1)
-# list = []
-# for i in range(edge_size):
-#     list.append(x[i].x)
-# print(list)
-
-# s_list = []
-# for j in range(vertex_size):
-#     s_list.append(s[j].x)
-# print(s_list)
-
-# 打印 path
-# for k in range(vehicle_num):
-#     print('EB_' + str(k) + ' ' + graph.vertex_set[k+1].vertex_attribute + ' soc =' + str(graph.vertex_set[k+1].soc) )
-#     path_k = []
-#     if x[k].x == 1:
-#         path_k.extend(graph.edge_set[k].vertex_id_pair)
-#         next_vertex_id = graph.edge_set[k].end2.vertex_id
-#         while len(graph.vertex_set[next_vertex_id].edge_id_out) != 0:
-#             for i in graph.vertex_set[next_vertex_id].edge_id_out:
-#                 if x[i].x == 1:
-#                     # path_k.append(str(graph.edge_set[i].end2.vertex_id) + ':' + str(s[graph.edge_set[i].end2.vertex_id].x))
-#                     path_k.append(
-#                         str(c[i].x) + ':' + str(graph.edge_set[i].end2.vertex_id))
-#                     # path_k.append(graph.edge_set[i].end2.vertex_id)
-#                     next_vertex_id = graph.edge_set[i].end2.vertex_id
-#                     break
-#     print(path_k)
-
-
-
-# print('time (optimization) = ', timer_optimize_end - timer_optimize_start)
+        iteration += 1
+        break
 
 
 
